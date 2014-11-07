7a98eef6ebe3306542117bf1323423677a3f78a8
cuijianwei <cuijianwei@cuijianwei-OptiPlex-990.(none)>
2014-11-07 09:18:23 +0800

    use a feek row to construct DataGet for themis index

--- themis-index/src/main/java/org/apache/hadoop/hbase/themis/index/cp/DataGet.java
+++ themis-index/src/main/java/org/apache/hadoop/hbase/themis/index/cp/DataGet.java
@@ -3,11 +3,13 @@ package org.apache.hadoop.hbase.themis.index.cp;
 import java.io.IOException;
 
 import org.apache.hadoop.hbase.themis.ThemisGet;
+import org.apache.hadoop.hbase.util.Bytes;
 
 // avoid set rowkey in ThemisGet
 public class DataGet extends ThemisGet {
+  private static final byte[] THEMIS_INDEX_DATA_GET_FEEK_ROW = Bytes.toBytes("__themis.feek.row__");
   public DataGet() {
-    super((byte[])null);
+    super(THEMIS_INDEX_DATA_GET_FEEK_ROW);
   }
   
   @Override
